ec8ca2253baf7b73bd8820c690f45534281baca5
kss <ksmit323@gmail.com>
2024-07-09 21:25:19 +0700

    update listen for events function

--- src/blockchain.rs
+++ src/blockchain.rs
@@ -13,6 +13,13 @@ use alloy::{
     sol_types::SolEvent,
 };
 
+sol!(
+    #[allow(missing_docs)]
+    #[sol(rpc)]
+    USDC,
+    "src/abi/usdc_abi.json",
+);
+
 pub struct BlockchainListener {
     provider: RootProvider<PubSubFrontend>,
     contract_address: Address,
@@ -25,10 +32,12 @@ impl BlockchainListener {
         contract_address: &str,
         // json_abi: &str,
     ) -> Result<Self, Box<dyn std::error::Error>> {
+
+        // Create the provider
         let ws = WsConnect::new(node_url);
         let provider = ProviderBuilder::new().on_ws(ws).await?;
-        let contract_address = contract_address.parse()?;
 
+        let contract_address = contract_address.parse()?;
         // let contract_abi: Abi = serde_json::from_str(json_abi)?;
 
         Ok(Self {
@@ -42,6 +51,7 @@ impl BlockchainListener {
         // Create a filter to watch for all contract events
         let filter = Filter::new()
             .address(self.contract_address)
+            .event("Transfer(address,address,uint256)")
             .from_block(BlockNumberOrTag::Latest);
 
         // Subscribe to logs
@@ -51,9 +61,51 @@ impl BlockchainListener {
         info!("Listening for blockchain events...");
 
         while let Some(log) = stream.next().await {
-            info!("Received event: {:?}", log);
+
+            // // Match on topic 0, the hash of the signature of the event.            
+            // match log.topic0() {
+            //     // Match the `Transfer(address,address,uint256)` event.
+            //     Some(&USDC::Transfer::SIGNATURE_HASH) => {
+            //         let USDC::Transfer { from, to, value } = log.log_decode()?.inner.data;
+            //         info!("Transfer Event: From: {from}, To: {to}, Amount; {value}");
+            //     }
+            //     _ => (),
+            // }
+
+            info!("Received event 111111: {log:?}");
+            info!("Received event 222222: {:?}", log.topic0());
+            info!("Received event 333333: {:?}", log.topics());
         }
 
         Ok(())
     }
+
+    // pub async fn process_event(&self, log: Log) -> Result<(), Box<dyn std::error::Error>> {
+    //     // Need to convert web3::types::Log to ethers::abi::RawLog
+    //     let raw_log = RawLog {
+    //         topics: log.topics,
+    //         data: log.data.0,
+    //     };
+
+    //     let event = self
+    //         .contract_abi
+    //         .event("Transfer")?
+    //         .parse_log(raw_log.into())?;
+
+    //     let from: EthersH160 = event.params[0].value.clone().into_address().unwrap();
+    //     let to: EthersH160 = event.params[1].value.clone().into_address().unwrap();
+    //     let amount: U256 = event.params[2].value.clone().into_uint().unwrap();
+
+    //     info!(
+    //         "Transfer Event: From: {:?}, To: {:?}, Amount: {}",
+    //         from, to, amount
+    //     );
+
+    //     // TODO: Add custom logic here:
+    //     // - Store the event in a database
+    //     // - Trigger notifications
+    //     // - update application state
+
+    //     Ok(())
+    // }
 }
